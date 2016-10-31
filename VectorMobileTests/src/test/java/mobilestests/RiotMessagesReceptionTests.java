a4a6734059910020a24ed12481e99a935bb36a6d
Jean Gouyou-Beauchamps <jeang@amdocs.com>
2016-10-31 12:08:02 +0100

    -Add tests checkAvatarDisplayInRoomPage and checkImageMessageOnRoomPage

--- VectorMobileTests/src/test/java/mobilestests/RiotMessagesReceptionTests.java
+++ VectorMobileTests/src/test/java/mobilestests/RiotMessagesReceptionTests.java
@@ -118,31 +118,58 @@ public class RiotMessagesReceptionTests extends testUtilities{
 		MobileElement beforeLastPost=  testRoom.postsListLayout.get(beforeLastPostPosition);
 		Assert.assertNull(testRoom.getTimeStampByPost(beforeLastPost), "Before last message have timestamp and should not.");
 		//select before last message
-		beforeLastPost.click();
+		testRoom.getTextViewFromPost(beforeLastPost).click();
 		//check that the timestamp is displayed
-		Assert.assertNotNull(testRoom.getTimeStampByPost(lastPost), "Before last message have no timestamp");
-		Assert.assertTrue(testRoom.getTimeStampByPost(lastPost).getText().length()>=5, "Before last message timestamp seems bad.");
+		//System.out.println(testRoom.getTextViewFromPost(beforeLastPost).getText());
+		Assert.assertNotNull(testRoom.getTimeStampByPost(beforeLastPost), "Before last message have no timestamp");
+		Assert.assertTrue(testRoom.getTimeStampByPost(beforeLastPost).getText().length()>=5, "Before last message timestamp seems bad.");
 	}
 	
 	/**
-	 * TODO write this test
 	 * Receive a message by an other user in a room.</br>
 	 * Send a first message. Check that avatar is displayed on the post.</br>
 	 * Send a second message. Check that avatar is not displayed on the post.
+	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	@Ignore
-	public void checkAvatarDisplayInRoomPage(){
-		
+	@Test(dependsOnGroups="roomOpenned",priority=4)
+	public void checkAvatarDisplayInRoomPage() throws IOException, InterruptedException{
+		String roomId="!ECguyzzDCnAZarUOSW%3Amatrix.org";
+		String senderAccesToken="MDAxOGxvY2F0aW9uIG1hdHJpeC5vcmcKMDAxM2lkZW50aWZpZXIga2V5CjAwMTBjaWQgZ2VuID0gMQowMDI1Y2lkIHVzZXJfaWQgPSBAamVhbmdiOm1hdHJpeC5vcmcKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMWRjaWQgdGltZSA8IDE0Nzc2NTg2MTAyNjEKMDAyZnNpZ25hdHVyZSAMRHy3V2nt7jDJlDrhq1NkEBBiHH6umGQvaydgqLcYlQo";
+		String messageTest="hello from sender";
+		String messageTest2="this message have an avatar";
+		String messageTest3="this message doesn't have an avatar";
+		//send a message to the room with an other user using https request to matrix.
+		HttpsRequestsToMatrix.sendMessageInRoom(senderAccesToken, roomId, messageTest);
+		RiotRoomPageObjects testRoom = new RiotRoomPageObjects(AppiumFactory.getAppiumDriver());
+		testRoom.sendAMessage(messageTest2);Thread.sleep(500);
+		Assert.assertNotNull(testRoom.getUserAvatarByPost(testRoom.getLastPost()), "The last post doesn't have an avatar and should because it's the first post from the user");
+		testRoom.sendAMessage(messageTest3);
+		Assert.assertNull(testRoom.getUserAvatarByPost(testRoom.getLastPost()), "The last post have an avatar and shouldn't because it's the second post from the user");
 	}
 	
 	/**
-	 * TODO write this test
 	 * Open a room, and receive a image sent by an other user.</br>
-	 * Check that image is correctly uploaded.
+	 * Check that image is correctly uploaded.</br>
+	 * Check that a timestamp is visible on the last post.
+	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	@Ignore
-	public void checkImageMessageOnRoomPage(){
-		
+	@Test(dependsOnGroups="roomOpenned",priority=5)
+	public void checkImageMessageOnRoomPage() throws IOException, InterruptedException{
+		String roomId="!ECguyzzDCnAZarUOSW%3Amatrix.org";
+		String pictureURL="mxc://matrix.org/gpQYPbjoqVeTWCGivjRshIni";
+		String senderAccesToken="MDAxOGxvY2F0aW9uIG1hdHJpeC5vcmcKMDAxM2lkZW50aWZpZXIga2V5CjAwMTBjaWQgZ2VuID0gMQowMDI1Y2lkIHVzZXJfaWQgPSBAamVhbmdiOm1hdHJpeC5vcmcKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMWRjaWQgdGltZSA8IDE0Nzc2NTg2MTAyNjEKMDAyZnNpZ25hdHVyZSAMRHy3V2nt7jDJlDrhq1NkEBBiHH6umGQvaydgqLcYlQo";
+		//send picture of already uploaded picture
+		HttpsRequestsToMatrix.sendPicture(senderAccesToken, roomId, pictureURL);
+		RiotRoomPageObjects testRoom = new RiotRoomPageObjects(AppiumFactory.getAppiumDriver());
+		Thread.sleep(500);
+		MobileElement lastPost=testRoom.getLastPost();
+		MobileElement uploadPicture = testRoom.getAttachedImageByPost(lastPost);
+		//get dimensions of the uploaded image
+		org.openqa.selenium.Dimension riotLogoDim=uploadPicture.getSize();
+	    Assert.assertTrue(riotLogoDim.height!=0 && riotLogoDim.width!=0, "Uploaded picture seems empty");
+	    Assert.assertNotNull(testRoom.getTimeStampByPost(lastPost), "Last post doesn't have a timestamp");
 	}
 
 	
--- VectorMobileTests/src/test/java/utility/HttpsRequestsToMatrix.java
+++ VectorMobileTests/src/test/java/utility/HttpsRequestsToMatrix.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -106,6 +108,53 @@ public class HttpsRequestsToMatrix {
 		   response.append(output);  
 		  }  
 		  in.close();  
+		  //printing result from response  
+		  System.out.println(response.toString());  
+	}
+	
+	/**
+	 * TODO : write this function
+	 * @param accessToken
+	 * @param roomId
+	 */
+	public static void uploadPicture(String accessToken, String roomId){
+		
+	}
+	
+	public static void sendPicture(String accessToken, String roomId, String pictureUrl) throws IOException{
+		  int token = 1 + (int)(Math.random() * ((10000 - 1) + 1));
+		  StringBuilder url=new StringBuilder(Constant.MATRIX_SERVER).append("/_matrix/client/r0/rooms/").append(roomId).append("/send/m.room.message/").append(token).append("?access_token=").append(accessToken);
+		  URL obj = new URL(url.toString());  
+		  HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		  con.setRequestMethod("PUT");
+		  con.setRequestProperty("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		  con.setRequestProperty("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		  con.setRequestProperty("Access-Control-Allow-Origin", "*");
+		  con.setRequestProperty("Content-Encoding", "gzip");
+		  con.setRequestProperty("Content-Type", "application/json");
+		  con.setRequestProperty("Server", "Synapse/0.18.2-rc1 (b=release-v0.18.2,t=v0.18.2-rc1,513e600)");
+		  con.setRequestProperty("Transfer-Encoding", "chunked");
+		  con.setRequestProperty("Date", "Mon, 24 Oct 2016 15:52:19 GMT");
+		  String postJsonData="{\"body\":\"small_koala.jpg\",\"info\":{\"size\":29051,\"mimetype\":\"image/jpeg\",\"w\":205,\"h\":154},\"msgtype\":\"m.image\",\"url\":\""+pictureUrl+"\"}";
+		  con.setDoOutput(true);  
+		  DataOutputStream wr = new DataOutputStream(con.getOutputStream());  
+		  wr.writeBytes(postJsonData);  
+		  wr.flush();
+		  wr.close();
+		  int responseCode = con.getResponseCode();
+		  System.out.println("\nSending 'POST' request to URL : " + url);
+		  System.out.println("Post Data : " + postJsonData);
+		  System.out.println("Response Code : " + responseCode);
+		  
+		  BufferedReader in = new BufferedReader(
+		          new InputStreamReader(con.getInputStream()));
+		  String output;
+		  StringBuffer response = new StringBuffer();  
+		  
+		  while ((output = in.readLine()) != null) {  
+		   response.append(output);  
+		  }  
+		  in.close();
 		  //printing result from response  
 		  System.out.println(response.toString());  
 	}
